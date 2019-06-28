b485f9d07d046645abb7e003db3641172928125b
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-06-28 19:49:35 +0200

    A user can view a project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -33,4 +33,9 @@ class ProjectsController extends Controller
 
         return redirect('/projects');
     }
+
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['title', 'description'];
+
+    /**
+     * Project has a path.
+     *
+     * @return string
+     */
+    public function path()
+    {
+        return '/projects/'.$this->id;
+    }
 }
